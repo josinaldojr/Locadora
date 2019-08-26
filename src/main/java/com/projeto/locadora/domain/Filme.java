9a5586a0187558ce0c9eab16c3e3be848cb939db
josinaldojr <josinaldo.pontes@dce.ufpb.br>
2019-08-26 10:22:06 -0300

    primeiro commit

---
+++ src/main/java/com/projeto/locadora/domain/Filme.java
@@ -0,0 +1,5 @@
+package com.projeto.locadora.domain;
+
+public class Filme {
+
+}
---
+++ src/main/java/com/projeto/locadora/domain/Telefone.java
@@ -0,0 +1,75 @@
+package com.projeto.locadora.domain;
+
+public class Telefone {
+	
+	private Integer id;
+	private String numero;
+	
+	private Cliente cliente;
+	
+	public Telefone() {}
+	
+	public Telefone(Integer id, String numero, Cliente cliente) {
+		super();
+		this.id = id;
+		this.numero = numero;
+		this.cliente = cliente;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Telefone other = (Telefone) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
